4e3d21035a42f64c726b1cfe4929053aac5a7936
Christine Xiang <yutingcxiang@gmail.com>
2018-07-16 01:25:26 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,8 +29,8 @@ class Transfer
       @receiver.balance -= @amount
       @status = "reversed"
     else
-      "Transaction rejected. Please check your account balance."
       @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
