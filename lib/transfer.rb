f21bc963a9a5a51960cd0101f5b5b426e39c3668
Christine Xiang <yutingcxiang@gmail.com>
2018-07-16 01:20:40 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? && @sender.balance > @amount && @status == "pending" 
+    if valid? && @sender.balance >= @amount && @status == "pending" 
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
