64adff60a0bb29be8f06cfac53cabdc593f11618
Christine Xiang <yutingcxiang@gmail.com>
2018-07-16 01:11:27 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,8 +29,8 @@ class Transfer
   
   def reverse_transfer
     if valid? == true && @status == "complete"
-      @sender.balance = @sender.balance + @amount
-      @receiver.balance = @receiver.balance - @amount
+      @sender.balance += @amount
+      @receiver.balance -= @amount
       @status = "reversed"
     else
       return "Transaction rejected. Please check your account balance."
