90d97ada073b870b0ba9e280991a52cbb463089b
Christine Xiang <yutingcxiang@gmail.com>
2018-07-16 01:11:03 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,8 @@ class Transfer
   
   def execute_transaction
     if valid? && @sender.balance > @amount && @status == "pending" 
-      @sender.balance = @sender.balance - @amount
-      @receiver.balance = @receiver.balance + @amount
+      @sender.balance -= @amount
+      @receiver.balance += @amount
       @status = "complete"
     else
       return "Transaction rejected. Please check your account balance."
